994a06e4bda481f587382d3c744ecf384a9628c7
Nabeel Mansour <59266812+NabeelMansour@users.noreply.github.com>
2023-10-23 17:52:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,28 +3,54 @@ const account = {
   expenses: [],
   income: [],
   addExpenses() {
+    let expenseType = prompt("What was your expense? (rent/gas/food etc)");
+    account.expenses.push(expenseType);
     let expense = parseFloat(prompt("Add your expense: "));
-    this.expenses.push(expense);
+    account.expenses.push(expense);
+    menu();
   },
   addIncome() {
     let userIncome = parseFloat(prompt("Add your income: "));
     this.income.push(userIncome);
+    menu();
   },
   listAllExpenses() {
-    expenses.forEach((element) => {
-      alert(``);
+    account.expenses.forEach((value, i) => {
+      alert(`Expense nr${i} ${value} - Amount ${account.expenses}`);
     });
-    alert(this.expenses);
+    menu();
   },
   getSummary() {
     alert();
   },
 };
 
-const menu = () => {
-  prompt(
-    "EXPENSE TRACKER\n Select a choise 1. Add income 2. add expense 3. List all expenses 4. Get summary"
-  );
-};
+console.log(account.expenses);
 
-menu();
+// const menu = () => {
+//   const choice = parseFloat(
+//     prompt(
+//       `EXPENSE TRACKER\n Select a choice ${1}. Add income ${2}. add expense ${3}. List all expenses ${4}. Get summary`
+//     )
+//   );
+
+//   switch (choice) {
+//     case 1:
+//       account.addIncome();
+//       break;
+//     case 2:
+//       account.addExpenses();
+//       break;
+//     case 3:
+//       account.listAllExpenses();
+//       break;
+//     case 4:
+//       account.getSummary();
+//       break;
+//     default:
+//       alert("Please choice from the menu");
+//       menu();
+//   }
+// };
+
+// menu();
