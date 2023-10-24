7526c3c59561acbf77485e2969a9b365a12989ab
Nabeel Mansour <59266812+NabeelMansour@users.noreply.github.com>
2023-10-24 13:36:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const account = {
   income: [],
   addExpenses() {
     let expenseType = prompt("What was your expense? (rent/gas/food etc)");
-    obj[expenseType] = account.expenses.label;
-    // account.expenses.push(expenseType);
+    account.expenses.push(expenseType);
     let expense = parseFloat(prompt("Add your expense: "));
-    obj[expense] = account.expenses.value;
-    account.expenses.push(obj);
-    // account.expenses.push({ expenseType: expense });
+    account.expenses.push({ expenseType: expense });
     menu();
   },
   addIncome() {
@@ -19,12 +16,7 @@ const account = {
   },
   listAllExpenses() {
     account.expenses.forEach((item, i) => {
-      console.log(
-        `Expense nr:` +
-          `${i + 1}: ${item[0].expenseType} - Amount ${item[0].expense}\n`
-      );
-      // console.log(element);
-      // alert(`Expense nr:` + `${i + 1}: ${element} - Amount ${element}\n`);
+      alert(`Expense nr:` + `${i + 1}: ${item} - Amount ${item}\n`);
     });
 
     menu();
